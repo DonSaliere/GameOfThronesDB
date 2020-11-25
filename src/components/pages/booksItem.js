4fee47ec78c8babed4dcd3467d080d17f9d451c4
Andrey <s.adnrey-klg@yandex.ru>
2020-11-25 23:29:13 +0300

    Правки

--- src/components/pages/booksItem.js
+++ src/components/pages/booksItem.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import gotService from '../../services/gotService';
-import CharDetails, { Field } from '../charDetails';
+import ItemDetails, { Field } from '../itemDetails';
 
 export default class BooksItem extends Component {
 	gotService = new gotService();
 
 	render() {
 		return (
-			<CharDetails
-				charId={this.props.bookId}
+			<ItemDetails
+				itemId={this.props.bookId}
 				getData={this.gotService.getBook}>
 				<Field field='numberOfPages' label='NumberOfPages' />
 				<Field field='publisher' label='Publisher' />
 				<Field field='released' label='Released' />
-			</CharDetails>
+			</ItemDetails>
 		)
 	}
 }
